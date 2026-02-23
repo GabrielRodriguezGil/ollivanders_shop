133a873509fa37ed5e57c72c2a4abf0ee8bcbf16
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-02-23 14:16:59 +0100

    feat(types): :sparkles: Añadir la clase OllivanderShop a el archivo que tendra los tipos de objetos de la tienda.

---
+++ src/types.py
@@ -0,0 +1,10 @@
+class OllivanderShop:
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            item.update_quality()
+
+    def getItems(self):
+        return self.items
