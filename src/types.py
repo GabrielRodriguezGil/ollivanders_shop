a8f4a5757442f36bf97a8d6c33ec10f24c9e3e6b
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-02-26 09:33:52 +0100

    feat(types): :sparkles: Añadir la clase NormalItem. y su comportamiento.

--- src/types.py
+++ src/types.py
@@ -20,3 +20,29 @@ class Item:
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
+
+    def __repr__(self):
+        return (
+            f"Item(name='{self.name}', sell_in={self.sell_in}, quality={self.quality})"
+        )
+
+
+class NormalItem(Interface, Item):
+    def __init__(self, name, sell_in, quality):
+        Item.__init__(self, name, sell_in, quality)
+
+    def get_quality(self):
+        return self.quality
+
+    def update_quality(self):
+        if self.quality == 0:
+            self._set_sell_in()
+        elif self.sell_in > 0:
+            self._set_sell_in()
+            self.quality -= 1
+        else:
+            self._set_sell_in()
+            self.quality -= 2
+
+    def _set_sell_in(self):
+        self.sell_in -= 1
